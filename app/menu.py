acfd5681564a7bfa3b35e5df3e61f098fcf76d53
gomjellie <gomjellie@naver.com>
2017-06-23 20:37:03 +0900

    \n in prettified str

--- app/menu.py
+++ app/menu.py
@@ -21,20 +21,20 @@ class Menu:
     def prettify(self, d, indent=0):
         if isinstance(d, dict):
             for key, value in d.items():
-                self.prettified_str += '├──' * (indent + 1) + str(key)
+                self.prettified_str += '\n├──' * (indent + 1) + str(key)
                 if isinstance(value, dict) or isinstance(value, list):
                     self.prettify(value, indent + 1)
                 else:
-                    self.prettified_str += '│  ├' + '─' * (indent + 1) + str(value)
+                    self.prettified_str += '\n│  ├' + '─' * (indent + 1) + str(value)
         elif isinstance(d, list):
             for item in d:
                 if isinstance(item, dict) or isinstance(item, list):
                     self.prettify(item, indent + 1)
                 else:
                     if item == d[-1]:
-                        self.prettified_str += '│  └' + '─' * (indent) + str(item)
+                        self.prettified_str += '\n│  └' + '─' * (indent) + str(item)
                     else:
-                        self.prettified_str += '│  ├' + '─' * (indent) + str(item)
+                        self.prettified_str += '\n│  ├' + '─' * (indent) + str(item)
         else:
             return Exception
 
