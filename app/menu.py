cb92cc21f01b29d2d062e2692396517a9392c228
gomjellie <gomjellie@naver.com>
2017-06-23 22:47:57 +0900

    improve food parsing, formatting

--- app/menu.py
+++ app/menu.py
@@ -21,7 +21,7 @@ class Menu:
     def prettify(self, d, indent=0):
         if isinstance(d, dict):
             for key, value in d.items():
-                self.prettified_str += '\n├──' * (indent + 1) + str(key)
+                self.prettified_str += '\n├─' * (indent + 1) + str(key)
                 if isinstance(value, dict) or isinstance(value, list):
                     self.prettify(value, indent + 1)
                 else:
@@ -50,22 +50,12 @@ class PupilMenu(Menu):
         self.foods = OrderedDict(sorted(unordered_food.items()))
 
     def get_dict(self):
-        ret_dict = {}
-        hangul = re.compile('[^가-힣 ]+')
-        for section in self.foods:
-            menu = self.foods[section][0]
-            english_removed = hangul.sub('', menu)
-            ret_dict.update({section: english_removed.split()})
-        return ret_dict
+        return self.foods
 
     def get_string(self):
         dic = self.get_dict()
         self.prettify(dic)
         ret_string = self.prettified_str
-        # dic = self.get_dict()
-        # for section in dic:
-        #     food_item = dic[section]
-        #     ret_string += section + ':' + ', '.join(food_item)
         return ret_string
 
 
@@ -80,23 +70,12 @@ class FacultyMenu(Menu):
         self.foods = OrderedDict(sorted(unordered_food.items()))
 
     def get_dict(self):
-        ret_dict = {}
-        hangul = re.compile('[^가-힣 ]+')
-        for section in self.foods:
-            menu = self.foods[section][0]
-            english_removed = hangul.sub('', menu)
-            ret_dict.update({section: english_removed.split()})
-
-        return ret_dict
+        return self.foods
 
     def get_string(self):
         dic = self.get_dict()
         self.prettify(dic)
         ret_string = self.prettified_str
-        # hangul = re.compile('[^가-힣 ]+')
-        # for section in self.foods:
-        #     menu = self.foods[section][0]
-        #     english_removed = hangul.sub('', menu)
-        #     ret_string += section + ':' + english_removed.rstrip() + '\n'
+
         return ret_string
 
