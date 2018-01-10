c84d6d90f01cef7829c06627cb3056c50f178aae
gomjellie <gomjellie@naver.com>
2018-01-10 17:38:57 +0900

    db리셋 스크립트 내일항목 추가

--- scripts/reset_today_data.py
+++ scripts/reset_today_data.py
@@ -6,4 +6,5 @@ food_db = conn.food_db
 hakusiku = food_db.hakusiku
 
 hakusiku.delete_one({'날짜': datetime.date.today().__str__()})
-
+tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+hakusiku.delete_one({'날짜': tomorrow.__str__()})
