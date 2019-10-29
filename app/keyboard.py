fdec4e58a2835fbcbc02bdefda3a87a02049a1b3
jacky <gomjellie@gmail.com>
2019-10-29 16:04:11 +0900

    remove 기식 button

--- app/keyboard.py
+++ app/keyboard.py
@@ -10,7 +10,7 @@ class Keyboard:
                     ]
 
     food_buttons = ['학식', '교식', '푸드코트', '스낵코너', '더 키친']
-    ratable_food_buttons = ['학식', '교식', '기식']
+    ratable_food_buttons = ['학식', '교식']
     rating_buttons = ['맛있음', '보통', '맛없음']
     review_buttons = ['리뷰 남기기']  # '리뷰 삭제하기']
     lib_buttons = ['1열람실', '2열람실', '3열람실', '4열람실']
